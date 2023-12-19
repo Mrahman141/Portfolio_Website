48834b7684d46e635bfebd087152236bca913f48
mrahman141 <mrahman141@myseneca.ca>
2023-12-18 20:22:07 -0500

    have navbar but not suitable for mobile devices

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        <Navbar />
+      <body className='bg-black'>
+        <Navbar/>
         {children}
       </body>
     </html>
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,5 +1,25 @@
 export default function Navbar() {
-  return (
-    <div className='bg-Midnight-Blue text-white'>Navbar</div>
-  )
+    return (
+        <nav className='bg-Midnight-Blue text-white'>
+            <div className="max-w-6xl mx-auto px-4">
+                <div className="flex justify-between">
+                    <a href="#" class="text-white text-lg font-bold">Aminoor</a>
+
+                    <div class="hidden sm:flex space-x-7">
+                        <a href="#" class="text-white">Home</a>
+                        <a href="#" class="text-white">About Me</a>
+                        <a href="#" class="text-white">Skills</a>
+                        <a href="#" class="text-white">Projects</a>
+                        <a href="#" class="text-white">Contact</a>
+                    </div>
+
+                    <div class="sm:hidden">
+                        <button class="text-white focus:outline-none">
+                            &#9776;
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
 }
