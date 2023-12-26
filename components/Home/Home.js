309441c1597769b7243544acb143556b5d226832
mrahman141 <mrahman141@myseneca.ca>
2023-12-26 13:39:41 -0500

    testing

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -10,6 +10,7 @@ export default function Home() {
                         <div className="md:shrink-0">
                             <img className="object-cover rounded-full md:h-128 md:w-128 lg:h-128 lg:w-128" src='/Me.png' alt="Mohammed Aminoor Rahman" />
                         </div>
+                        
                         <div className="p-8 text-white text-2xl md:text-3xl lg:text-4xl">
                             <p className="mt-2">Hi there! I&apos;m Mohammed Aminoor Rahman, a Software Developer studying at Seneca Polytechnic.</p>
                             <p className="mt-2">Welcome to my website!</p>
